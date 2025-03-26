d356048cea90f3730ce5e1c6b189a1e2b5d92cdf
m.uppenkamp <35469083+MUppenkamp@users.noreply.github.com>
2025-03-26 15:21:19 +0100

    fix tailwind config

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,10 +1,5 @@
-// import { tailwindLayouts, defaultOptions } from 'tailwind-layouts';
-const tailwindLayouts = require('tailwind-layouts')
+import { tailwindLayouts, defaultOptions } from 'tailwind-layouts';
 
-/** @type {import('tailwindcss').Config} */
-export const content = [
-  './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-]
 export const theme = {
   extend: {
     fontFamily: require('./tailwind/fontFamily.cjs'),
@@ -13,33 +8,17 @@ export const theme = {
     typography: require('./tailwind/typography.cjs'),
   },
 }
+
 export const plugins = [
   require('./tailwind/base.cjs'),
-  require('@tailwindcss/typography'),
-  tailwindLayouts,
-  // tailwindLayouts({
-  //   ...defaultOptions,
-  //   useLogicalProperties: true,
-  //   classNames: {
-  //     ...defaultOptions.classNames,
-  //     center: 'el-center',
-  //     cluster: 'el-cluster',
-  //     cover: 'el-cover',
-  //     imposter: 'el-imposter',
-  //     reel: 'el-reel',
-  //     sidebar: 'el-sidebar',
-  //     stack: 'el-stack',
-  //     switcher: 'el-switcher',
-  //     grid: 'el-grid',
-  //   },
-  // })
-  // tailwindLayouts({
-  //   ...defaultLayoutOptions,
-  //   useGlobalScrollbarStyles: true,
-  //   measureExceptions: [
-  //     defaultLayoutOptions.measureExceptions,
-  //     '.prose *:not(img):not(video)',
-  //   ].join(','),
-  // }),
+  tailwindLayouts({
+    ...defaultOptions,
+    useGlobalScrollbarStyles: true,
+    measureExceptions: [
+      defaultOptions.measureExceptions,
+      '.prose *:not(img):not(video)',
+    ].join(','),
+  }),
 ]
-export const darkMode = 'class'
+
+export const darkMode = 'class';
