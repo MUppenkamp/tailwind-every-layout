872a6efa17bb92952926d25c79522afd618c4ed8
m.uppenkamp <35469083+MUppenkamp@users.noreply.github.com>
2025-03-26 16:11:29 +0100

    fix plugin and some types

--- tailwind-layouts/lib/main.ts
+++ tailwind-layouts/lib/main.ts
@@ -1,4 +1,5 @@
-const plugin = require('tailwindcss/plugin');
+// @ts-ignore Tailwind does not provide correct types 😔
+import plugin, { PluginAPI } from 'tailwindcss/plugin'
 import { PLUGIN_NAMESPACE } from './constants'
 import { t } from './helpers'
 import defaultOptions from './options'
@@ -19,73 +20,11 @@ import imposter from './utilities/layouts/imposter'
 import icon from './utilities/layouts/icon'
 import scrollbar from './utilities/scrollbar'
 import borderHC from './utilities/border-hc'
-import type { PluginOptions, Utility } from './types'
+import type { PluginOptions, PluginTheme, Utility } from './types'
 
-// const tailwindLayouts = plugin.withOptions<PluginOptions>(
-//   (options = defaultOptions) =>
-//     ({ addBase, addUtilities, matchUtilities, theme }) => {
-//       if (options.useGlobalMeasure) {
-//         addBase(...globalMeasure({ options, theme }))
-//       }
-//       if (options.useGlobalScrollbarStyles) {
-//         addBase(...globalScrollbar({ options, theme }))
-//       }
-//
-//       const registerUtility = (utility: Utility) => {
-//         if (utility.static) {
-//           for (const u of utility.static) {
-//             addUtilities(...u({ options, theme }))
-//           }
-//         }
-//         if (utility.dynamic) {
-//           for (const u of utility.dynamic) {
-//             matchUtilities(...u({ options, theme }))
-//           }
-//         }
-//       }
-//
-//       registerUtility(stack)
-//       registerUtility(box)
-//       registerUtility(center)
-//       registerUtility(cluster)
-//       registerUtility(sidebar)
-//       registerUtility(switcher)
-//       registerUtility(cover)
-//       registerUtility(grid)
-//       registerUtility(frame)
-//       registerUtility(reel)
-//       registerUtility(imposter)
-//       registerUtility(icon)
-//
-//       registerUtility(scrollbar)
-//       registerUtility(borderHC)
-//     },
-//
-//     (options = defaultOptions) => ({
-//       theme: {
-//         [PLUGIN_NAMESPACE]: defaultTheme,
-//         extend: {
-//           spacing: ({ theme }) => ({
-//             [options.baseSpacingKey]: `${t(theme, 'baseSpacing')}`,
-//             [options.measureKey]: `${t(theme, 'measure')}`,
-//           }),
-//           maxWidth: ({ theme }) => ({
-//             [options.measureKey]: `${t(theme, 'measure')}`,
-//           }),
-//           minWidth: ({ theme }) => ({
-//             [options.measureKey]: `${t(theme, 'measure')}`,
-//           }),
-//         },
-//       },
-//     })
-// )
-
-// const tailwindLayouts = plugin.withOptions<PluginOptions>(
-const tailwindLayouts = plugin.withOptions(
-// @ts-ignore
+const tailwindLayouts = plugin.withOptions<PluginOptions>(
   (options  = defaultOptions) => {
-// @ts-ignore
-    return ({ addBase, addUtilities, matchUtilities, theme }) => {
+    return ({ addBase, addUtilities, matchUtilities, theme }: PluginAPI) => {
       if (options.useGlobalMeasure) {
         addBase(...globalMeasure({ options, theme }))
       }
@@ -95,13 +34,11 @@ const tailwindLayouts = plugin.withOptions(
       const registerUtility = (utility: Utility) => {
         if (utility.static) {
           for (const u of utility.static) {
-            // @ts-ignore
             addUtilities(...u({ options, theme }))
           }
         }
         if (utility.dynamic) {
           for (const u of utility.dynamic) {
-            // @ts-ignore
             matchUtilities(...u({ options, theme }))
           }
         }
@@ -125,29 +62,27 @@ const tailwindLayouts = plugin.withOptions(
   (options = defaultOptions) => ({
     theme: {
       [PLUGIN_NAMESPACE]: defaultTheme,
-      extend: {
-// @ts-ignore
-        spacing: ({ theme }) => ({
-          [options.baseSpacingKey]: `${t(theme, 'baseSpacing')}`,
-          [options.measureKey]: `${t(theme, 'measure')}`,
-        }),
-// @ts-ignore
-        maxWidth: ({ theme }) => ({
-          [options.measureKey]: `${t(theme, 'measure')}`,
-        }),
-// @ts-ignore
-        minWidth: ({ theme }) => ({
-          [options.measureKey]: `${t(theme, 'measure')}`,
-        }),
-        colors: {
-          slay: '#ff42d3'
-        },
+      extend: ({ theme }: PluginAPI) => {
+        return {
+          spacing: {
+            [options.baseSpacingKey]: `${t(theme, 'baseSpacing')}`,
+            [options.measureKey]: `${t(theme, 'measure')}`,
+          },
+          maxWidth: {
+            [options.measureKey]: `${t(theme, 'measure')}`,
+          },
+          minWidth: {
+            [options.measureKey]: `${t(theme, 'measure')}`,
+          },
+          colors: {
+            slay: '#ff69b4',
+          },
+        };
       },
     },
   })
 )
 
-export { tailwindLayouts };
-// export { tailwindLayouts, defaultOptions, defaultTheme }
-// export type Options = Partial<PluginOptions>
-// export type Theme = Partial<PluginTheme>
+export { tailwindLayouts, defaultOptions, defaultTheme }
+export type Options = Partial<PluginOptions>
+export type Theme = Partial<PluginTheme>
