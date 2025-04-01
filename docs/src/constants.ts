67d11ae86649c87f82ffe627d9bd2476c9be2235
m.uppenkamp <35469083+MUppenkamp@users.noreply.github.com>
2025-04-01 11:18:52 +0200

    fix wrong urls

--- docs/src/constants.ts
+++ docs/src/constants.ts
@@ -3,8 +3,8 @@ import type { NavLinks, PathName } from './types'
 export const SITE_TITLE = 'Tailwind Every Layout'
 export const SITE_DESCRIPTION = 'Layout primitives for Tailwind CSS.'
 export const GITHUB_URL = 'https://github.com/MUppenkamp/tailwind-every-layout'
-export const DEFAULT_THEME_URL = `${GITHUB_URL}/blob/main/tailwind-layouts/lib/theme.ts`
-export const DEFAULT_OPTIONS_URL = `${GITHUB_URL}//blob/main/tailwind-layouts/lib/options.ts`
+export const DEFAULT_THEME_URL = `${GITHUB_URL}/blob/main/tailwind-every-layout/lib/theme.ts`
+export const DEFAULT_OPTIONS_URL = `${GITHUB_URL}/blob/main/tailwind-every-layout/lib/options.ts`
 
 export const PRIMARY_LINKS: NavLinks<PathName> = [
   { slug: 'usage', text: 'Usage' },
