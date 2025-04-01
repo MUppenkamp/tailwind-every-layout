f52465a6b73dfcd7d5d984924946af99f19a590a
m.uppenkamp <35469083+MUppenkamp@users.noreply.github.com>
2025-04-01 11:02:09 +0200

    remove twitter author

--- docs/src/constants.ts
+++ docs/src/constants.ts
@@ -5,7 +5,6 @@ export const SITE_DESCRIPTION = 'Layout primitives for Tailwind CSS.'
 export const GITHUB_URL = 'https://github.com/MUppenkamp/tailwind-every-layout'
 export const DEFAULT_THEME_URL = `${GITHUB_URL}/blob/main/tailwind-layouts/lib/theme.ts`
 export const DEFAULT_OPTIONS_URL = `${GITHUB_URL}//blob/main/tailwind-layouts/lib/options.ts`
-export const AUTHOR_TWITTER = 'aseemtaneja'
 
 export const PRIMARY_LINKS: NavLinks<PathName> = [
   { slug: 'usage', text: 'Usage' },
